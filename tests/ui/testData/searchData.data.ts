b387faf2887618d0bb4ee275f475c8c1f7bc1a7c
Alek_Sven <aleksandras.konygin@gmail.com>
2026-02-09 13:15:51 +0200

    add data file and data interface

---
+++ tests/ui/testData/searchData.data.ts
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+import { parse } from 'csv-parse/sync';
+
+interface product {
+    ITEM_CATEGORY: string
+}
+
+interface searchData extends Array<product>{}
+
+
+const records = parse(fs.readFileSync(
+    path.join(  `${__dirname}/dataFiles`, 'search.csv')),
+                {
+                    columns: true,
+                    skip_empty_lines: true
+                }
+            );
+
+
+export const searchData = records as searchData
+
+
+
+
