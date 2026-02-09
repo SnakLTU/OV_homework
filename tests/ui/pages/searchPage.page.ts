81924416f5e5fa6656dd3fb80895bcca21c33914
Alek_Sven <aleksandras.konygin@gmail.com>
2026-02-09 23:51:16 +0200

    test for brand test for price

--- tests/ui/pages/searchPage.page.ts
+++ tests/ui/pages/searchPage.page.ts
@@ -3,15 +3,78 @@ import { type Page, type Locator } from "@playwright/test";
 export class SearchPage {
     readonly page: Page;
     readonly searchResults: Locator
+    readonly submitRangeButton: Locator
+    readonly minPriceField: Locator
+    readonly maxPriceField: Locator
 
 
     constructor(page: Page) {
         this.page = page;
-        this.searchResults = this.page.locator('ul[class="srp-results srp-list clearfix"]')
-        
+        this.searchResults = this.page.locator('ul[class="srp-results srp-list clearfix"]');
+        this.minPriceField = this.page.getByRole('textbox', { name: 'Minimum Value in $' });
+        this.maxPriceField = this.page.getByRole('textbox', { name: 'Maximum Value in $' });
+        this.submitRangeButton = this.page.getByRole('button', { name: 'Submit price range' });
     };
 
-    async waitForSearchResults(productName: string) {
-        await this.page.waitForURL(new RegExp(`\\/sch\\/i.html\\?_nkw=${productName}&`)); 
+    async waitForSearchResults(productName: string){
+        return this.page.waitForURL(new RegExp(`${productName}`));
+    };
+
+    async waitForImagesToLoad(){
+        return  this.page.waitForResponse(response =>
+                response.url().includes('multiple_images_expsvc') && response.status() === 200
+        );
+    };
+
+    async waitForUserAcquisition(){
+        return  this.page.waitForResponse(response =>
+                response.url().includes('useracquisition') && response.status() === 200
+        );
+    };
+
+    async clickBrandFilterCheckBox(brand: string){
+        const brandCheckbox = this.page.getByLabel( brand, { exact: true });
+        await brandCheckbox.click();
+        return brandCheckbox;
+    };
+
+    async fillMinPriceTextField(price: string){
+        await this.minPriceField.pressSequentially(price);
+    };
+
+    async fillMaxPriceTextField(price: string){
+        await this.minPriceField.pressSequentially(price);
+    };
+
+    async clickSubmitRangeButton(){
+        await this.submitRangeButton.click();
+    };
+
+    async collectSearchResults(range: number){
+        const collectedResults: (string | null)[] = Array()
+        const resultsList = this.searchResults.locator('li');
+        for (let i=0; i<range; i++){
+            collectedResults.push(  await resultsList.nth(i)
+                                    .locator('span[class="su-styled-text primary default"]')
+                                    .textContent()
+                                )
+        };
+        return collectedResults;
+    };
+
+    async collectSearchResultPrices(range: number){
+        const collectedResults: (number | null)[] = Array()
+        const resultsList = this.searchResults.locator('li');
+        for (let i=0; i<range; i++){
+            let priceText = await resultsList.nth(i).locator('div[class="s-card__attribute-row"]')
+                                                  .first()
+                                                  .textContent()
+            if (priceText) {
+                priceText = priceText.replace('$', '')
+                const price = parseFloat(priceText)
+                collectedResults.push(price)
+            }
+        };
+        return collectedResults
     }
 };
--- tests/ui/specs/func/search.spec.ts
+++ tests/ui/specs/func/search.spec.ts
@@ -1,23 +1,76 @@
 import { test, expect } from '../base.ts';
 import { searchData } from '../../testData/searchData.data.ts';
+import { SearchPage } from '../../pages/searchPage.page.ts';
 
 
 test.describe('Search and filter functionality Test Suite', () => {
 
+    test.beforeEach(async ({homePage}) => {
+        await homePage.goto() //Step 1: Navigate to home page
+        await expect(homePage.searchBox).toBeVisible()  //Step 2: Validate that seach box is loaded and visible
+    })
+
     searchData.forEach(product => {
 
         test(`Validating search results from homePage, product ${product.ITEM_CATEGORY}`, 
-            async ({homePage,
-                    searchPage
-             }) => {
-            await homePage.goto() //Step 1: Navigate to home page
-            await expect(homePage.searchBox).toBeVisible()  //Step 2: Validate that seach box is loaded and visible
-            await homePage.typeProductToSearchBox(product.ITEM_CATEGORY) //Step 3: Type product category
-            await homePage.clickSearchButton() //Step 4: Click search button
-            await searchPage.waitForSearchResults(product.ITEM_CATEGORY) //Step 5: Wait to Navigate to search page URL
-            await expect(searchPage.searchResults).toBeVisible() //Step 6: Validate that search results are loaded
-            }
+            async ({
+                homePage,
+                searchPage
+            }) => {
+                const pageLoadRequestCategory = searchPage.waitForSearchResults(product.ITEM_CATEGORY)
+                await homePage.typeProductToSearchBox(product.ITEM_CATEGORY) //Step 3: Type product category
+                await homePage.clickSearchButton() //Step 4: Click search button
+                await pageLoadRequestCategory //Step 5: Wait to Navigate to search page URL
+                await expect(searchPage.searchResults).toBeVisible() //Step 6: Validate that search results are loaded
+                const searchResults = await searchPage.collectSearchResults(1); //Step 7: Validate that search returned correct category
+                searchResults.forEach(result => {
+                    expect.soft(result).toContain(product.ITEM_CATEGORY);
+                    });
+                }
         );
+
+        test(`Validate search results, using brand filter: ${product.ITEM_BRAND}`,
+            async ({
+                homePage,
+                searchPage
+            }) => {
+                const pageLoadRequestCategory = searchPage.waitForSearchResults(product.ITEM_CATEGORY)
+                await homePage.typeProductToSearchBox(product.ITEM_CATEGORY) //Step 3: Type product category
+                await homePage.clickSearchButton() //Step 4: Click search button
+                await pageLoadRequestCategory //Step 5: Wait to Navigate to search page URL
+                const checkBox = await searchPage.clickBrandFilterCheckBox(product.ITEM_BRAND) //Step 6: Check Brand checkbox
+                await expect(checkBox).toBeChecked() //Step 8: Validate that brand checkbox is checked
+                await expect(searchPage.searchResults).toBeVisible()
+                const searchResults = await searchPage.collectSearchResults(1); //Step 7: Validate that search returned correct category
+                searchResults.forEach(result => {
+                    expect(result).toContain(product.ITEM_BRAND);
+                    });
+                }
+        );
+
+        test(`Validate search results, using price range filter min:${product.MIN_PRICE} price`,
+            async ({
+                homePage,
+                searchPage
+            }) => {
+                const imageLoadRequest = searchPage.waitForImagesToLoad()
+                await homePage.typeProductToSearchBox(product.ITEM_CATEGORY) //Step 3: Type product category
+                await homePage.clickSearchButton() //Step 4: Click search button
+                await searchPage.waitForSearchResults(product.ITEM_CATEGORY) //Step 5: Wait to Navigate to search page URL
+                await imageLoadRequest
+                await searchPage.fillMinPriceTextField(product.MIN_PRICE) //Step 6: Fill in minimum price
+                await expect(searchPage.submitRangeButton).toBeEnabled() //Step 7: Validate submit button to be enabled
+                await searchPage.clickSubmitRangeButton(); //Step 8: Submit minimum price
+                await searchPage.waitForSearchResults(`_udlo=${product.MIN_PRICE}`) //Step 9: Wait to Navigate to search page URL
+                await expect(searchPage.searchResults).toBeVisible()
+                const searchResultPrices = await searchPage.collectSearchResultPrices(5)
+                searchResultPrices.forEach(price => {
+                    expect(price).toBeGreaterThan(Number(product.MIN_PRICE))
+                })
+                await expect(searchPage.submitRangeButton).toBeEnabled() //Step 7: Validate submit button to be enabled
+            }
+        )
+
     });
 
 
